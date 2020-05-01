3dd8ee2cad38510ca306149561e7ee01083af445
Edwin García <spark.com.co@gmail.com>
2020-05-01 01:54:41 -0500

    Trapping rain water algorithm

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Algorithm that calculates the amount of trapped water given an array that represents the height on a topographic map.
 
+## Run
+
+You can edit array on top of code and run this to see the result
+
+```shell
+npm start
+```
+
 ## Author
 
 Edwin García  
--- index.js
+++ index.js
@@ -1,4 +1,46 @@
 const topographicMap = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+const topographicMap2 = [3, 0, 0, 2, 0, 4]
+const topographicMap3 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2]
+
+console.log(trappedWater(topographicMap))
+
+function trappedWater(elevationData) {
+    if(elevationData.length < 3){
+        return 0
+    }
+    const sortedData = mergeSort(elevationData)
+    const higherHeight = sortedData.shift()
+    const nextHeight = sortedData.shift()
+    let firstWall, lastWall
+    let water = 0
+
+    for(let i=0; i<elevationData.length; i++) {
+        const elevation = elevationData[i]
+        if(elevation === higherHeight || elevation === nextHeight) {
+            firstWall = i
+            break
+        }
+    }
+
+    for(let i=elevationData.length-1; i>=0; i--) {
+        const elevation = elevationData[i]
+        if(elevation === higherHeight || elevation === nextHeight) {
+            lastWall = i
+            break
+        }
+    }
+
+    for(let i=firstWall+1; i<lastWall; i++) {
+        const dammedWater = nextHeight - elevationData[i]
+        if(dammedWater > 0) {
+            water += dammedWater
+        }
+    }
+
+    elevationData.splice(firstWall, (lastWall - firstWall))
+
+    return water + trappedWater(elevationData)
+}
 
 function mergeSort(data) {
     const list = [...data]
@@ -23,7 +65,3 @@ function mergeSort(data) {
         return list
     }
 }
-
-console.log(mergeSort(topographicMap))
-
-console.log(topographicMap)
